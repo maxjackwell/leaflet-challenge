dcfcdd64d3143c2702a49a2cb3d7c5ce38ed19e1
Jack Maxwell <jackmaxwell@Jacks-MBP.mediacom.info>
2024-04-08 16:49:09 -0500

    Coordinates and Legend: All systems go.

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # leaflet-challenge
-
-
-
-
-
-
+Challenge 15 looks at an API focusing in on Earthquakes around the world. In this session, I decided to look at all of the earthquakes recorded in the last 30 days. I visualized them with the color describing the depth of the Earthquake, and the size of the circle relates to the overall magnitude of the Earthquake. Looking at the HTML, this map lights up like a Christmas tree, especially along fault lines. We could have changed the graphic to only look at certain Magnitudes or even included data to show if Tsunamis occured in response to the EQ. 
+I could not have completed this challenge without previous class notes, the AskBCS tutors and the following links:
 
 https://rstudio.github.io/leaflet/articles/legends.html#:~:text=Use%20the%20addLegend%20function%20to,colors%20and%20labels%20for%20you.
 https://gis.stackexchange.com/questions/133630/adding-leaflet-legend
+https://stackoverflow.com/questions/5630574/style-legend-tag-as-block
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,14 +1,11 @@
 // Part 1: Create the Earthquake Visualization
-
 // Data Retrieval
 // All Earthquakes - Past 30 Days -
 let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
-
 // Data Processing
 d3.json(url).then(function (data) {
     createFeatures(data.features);
 });
-
 // Create Features
 function createFeatures(eqData) {
     // Function to create popup content for each earthquake feature
@@ -18,25 +15,22 @@ function createFeatures(eqData) {
             <h3>Location:</h3><p>${feature.properties.place}</p><hr>
             <h4>Time:</h4><p>${new Date(feature.properties.time)}</p>`);
     }
-    
-
     // Function to choose color based on earthquake depth
     function chooseColor(depth) {
         if (depth > -10 && depth < 10) {
-            return "#0bda51"; // light green
+            return "#0BDA51"; // light green
         } else if (depth >= 10 && depth < 30) {
-            return "#03c03c"; //  green
+            return "#03C03C"; //  green
         } else if (depth >= 30 && depth < 50) {
-            return "#ecb653"; // light orange
+            return "#ECB653"; // light orange
         } else if (depth >= 50 && depth < 70) {
-            return "#ff8c00"; // orange
+            return "#FF8C00"; // orange
         } else if (depth >= 70 && depth < 90) {
-            return "#c32c78"; // pink
+            return "#8B4000"; // dark orange
         } else if (depth >= 90) {
-            return "#841617"; // red
+            return "#FF0000"; // red
         }
     }
-
     // Create the earthquake layer
     let earthquakes = L.geoJSON(eqData, {
         pointToLayer: function (feature, latlng) {
@@ -51,44 +45,37 @@ function createFeatures(eqData) {
         },
         onEachFeature: onEachFeature
     });
-
     // Create the map
     createMap(earthquakes);
 }
-
 // Create Map
 function createMap(eq) {
     // Create the base tile layer
     let topo = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     });
-
     // Define base and overlay maps
     let baseMaps = { "Topographic Map": topo };
     let overlayMaps = { "Earthquakes In the Last 30 Days": eq };
-
     // Create the map with layers and controls
     let myMap = L.map("map", {
-        center: [34.0549, 118.2426],
+        center: [40.7, -94.5],
         zoom: 3,
         layers: [topo, eq]
     });
-
     // Add legend
     let legend = L.control({ position: "bottomright" });
-    legend.onAdd = function (map) {
+    legend.onAdd = function() {
         let div = L.DomUtil.create("div", "info legend");
-        let depths = [-10, 10, 30, 50, 70, 90];
-        let labels = [];
-
-        for (let i = 0; i < depths.length; i++) {
-            div.innerHTML +=
-                '<i style="background:' + chooseColor(depths[i] + 1) + '"></i> ' +
-                depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+        let grades = [-10, 10, 30, 50, 70, 90];
+        let colors = ["#0BDA51", "#03C03C", "#ECB653", "#FF8C00", "#8B4000", "#FF0000"];
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML += "<i style='background: " + colors[i] + "'></i> " +
+                grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
         }
         return div;
     };
     legend.addTo(myMap);
-
+    // Add layer control
     L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(myMap);
 }
